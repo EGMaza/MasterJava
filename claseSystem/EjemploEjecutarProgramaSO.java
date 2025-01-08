a1bb9c32a4c6d8c14215d067ed01b2839758c5ea
Eduardo compuTrabajo <eduardo_gonzalez_maza@hotmail.com>
2025-01-08 12:49:31 -0600

    Se agregó la clase 'EjemploejecutarProgramaSO' donde se ve un poco del funcionamiento de la clase Runtime

---
+++ claseSystem/EjemploEjecutarProgramaSO.java
@@ -0,0 +1,39 @@
+package claseSystem;
+
+import java.io.IOException;
+
+public class EjemploEjecutarProgramaSO {
+    public static void main(String[] args) {
+        Runtime rt = Runtime.getRuntime();
+        Process proceso;
+
+        try{
+            if(System.getProperty("os.name").toLowerCase().startsWith("windows")){
+                proceso = rt.exec("notepad");
+            }
+            else if(System.getProperty("os.name").toLowerCase().startsWith("mac")){
+                proceso = rt.exec("textedit");
+            }
+            else if(System.getProperty("os.name").toLowerCase().contains("nix")||
+            System.getProperty("os.name").toLowerCase().contains("nux")){
+                proceso = rt.exec("gedit");
+            }
+            else{
+                proceso = rt.exec("gedit");
+            }
+            proceso.waitFor();
+    }catch(IOException e){
+        System.err.println("El comando es desconocido " + e.getMessage());
+        System.exit(1);
+    }catch(InterruptedException e){
+        System.err.println("El comando es desconocido " + e.getMessage());
+        System.exit(1);
+    }
+
+    System.out.println("Se ha cerrado el editor");
+    //Nota: la clase Runtime también se puede ocupar para ejecutar comandos de la consola de comandos        
+
+
+
+    }
+}
