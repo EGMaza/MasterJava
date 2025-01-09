09e728c9547dd4d6f271f59d58f4e16afcf0bdde
Eduardo compuTrabajo <eduardo_gonzalez_maza@hotmail.com>
2025-01-09 17:52:45 -0600

    Se creó la clase 'EjemploArreglosOrdenPrincipioFinal'

---
+++ src/Arreglos/EjemploArreglosOrdenPrincipioFinal.java
@@ -0,0 +1,25 @@
+package Arreglos;
+
+public class EjemploArreglosOrdenPrincipioFinal {
+    public static void main(String[] args) {
+        int[] numeros = new int[10];
+        int[] a = new int[10];
+
+        for(int i=0;i<numeros.length;i++){
+            numeros[i] = i +1 ;
+        }
+        int cont = 0;
+        for(int i=0;i<numeros.length-i;i++){
+
+            //System.out.print(numeros[i] + " ");
+            //System.out.print(numeros[numeros.length-1-i] + " ");
+            a[cont++]=numeros[i];
+            a[cont++]=numeros[numeros.length-1-i];
+        }
+
+        for(int i=0;i<a.length;i++){
+            System.out.print(a[i] + " ");
+        }        
+    }
+
+}
