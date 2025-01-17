3b91a16e75ce37de4f0d2dd53f87c7914e376d65
Eduardo compuTrabajo <eduardo_gonzalez_maza@hotmail.com>
2025-01-17 09:53:27 -0600

    Se creó la clase 'EjemploAutomovilEnum.java' y se actualizó el enumerador 'TipoAutomovil.java'

---
+++ src/ProgramacionOrientadaAObjetos/EjemploAutomovilEnum.java
@@ -0,0 +1,65 @@
+package ProgramacionOrientadaAObjetos;
+
+public class EjemploAutomovilEnum {
+    public static void main(String[] args) {
+
+        Automovil.setCapacidadEstanqueEstatico(45);
+        Automovil subaru = new Automovil("Subaru","Impreza");
+        subaru.setCilindrada(2.0);
+        subaru.setColor(Color.BLANCO);
+        subaru.setTipo(TipoAutomovil.HATCHBACK);
+
+        Automovil mazda = new Automovil("Mazda","BT-50",Color.ROJO,3);
+        mazda.setTipo(TipoAutomovil.PICKUP);
+        System.out.println("mazda.fabricante = " + mazda.getFabricante());
+        //Automovil.setColorPatente(Color.NARANJA);
+        //System.out.println(subaru.verDetalle());
+        //System.out.println(mazda.verDetalle());
+
+        TipoAutomovil tipo = subaru.getTipo();
+        tipo = mazda.getTipo();
+        System.out.println("Tipo subaru = " + tipo.getNombre());
+        System.out.println("Tipo desc. subaru = " + tipo.getDescripcion());
+
+        switch (tipo) {
+            case CONVERTIBLE:
+                System.out.println("El automovil es deportivo y descapotable de dos puertas");
+            break;
+
+            case COUPE:
+                System.out.println("Es un automóvil de dos puertas y típicamente deportivo");
+            break;
+
+            case FURGON:
+                System.out.println("Es un automóvil utilitario de transporte, de empresas");
+            break;
+
+            case HATCHBACK:
+                System.out.println("Es un automóvil mediano compacto, aspecto deportivo");
+            break;
+            
+            case PICKUP:
+                System.out.println("Es un automóvil de doble cabina o camioneta");
+            break;
+
+            case SEDAN:
+                System.out.println("Es un automóvil mediano");
+            break;
+
+            case STATION_WAGON:
+                System.out.println("Es un automóvil mas grande, con maleta grande");
+            break;
+        }
+
+        TipoAutomovil[] tipos = TipoAutomovil.values();
+
+        for(TipoAutomovil ta:tipos){
+            System.out.print(ta + " => " + ta.name() + 
+            ", " + ta.getNombre() +
+            ", " + ta.getDescripcion() +
+            ", " + ta.getNumeroPuertas());
+
+            System.out.println();
+        }
+    }
+}
