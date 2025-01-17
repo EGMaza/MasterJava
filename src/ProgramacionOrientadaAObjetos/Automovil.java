39f398d006eef3a6e2132ae9295290d9c45195fd
Eduardo compuTrabajo <eduardo_gonzalez_maza@hotmail.com>
2025-01-16 22:59:53 -0600

    Se modificaron las clases 'EjemploAutomovil.java' y 'Automovil.java' segun se iban usando en los videos

--- src/ProgramacionOrientadaAObjetos/Automovil.java
+++ src/ProgramacionOrientadaAObjetos/Automovil.java
@@ -2,9 +2,164 @@ package ProgramacionOrientadaAObjetos;
 
 class Automovil {
 
-    String fabricante;
-    String modelo;
-    String color;
-    double cilindrada;
+    private String fabricante;
+    private String modelo;
+    private Color color = Color.GRIS_OSCURO;
+    private double cilindrada;
+    private int capacidadEstanque = 40;
+    private int id;
 
+    private static int capacidadEstanqueEstatico = 30;
+    private static Color colorPatente = Color.NARANJA;
+    private static int ultimoId;
+
+    public static final Integer VELOCIDAD_MAX_CARRETERA = 120;
+    public static final int VELOCIDAD_MAX_CIUDAD = 60;
+    public static final String COLOR_ROJO = "rojo";
+    public static final String COLOR_AMARILLO = "amarillo";
+    public static final String COLOR_AZUL = "azul";
+    public static final String COLOR_BLANCO = "blanco";
+    public static final String COLOR_GRIS = "gris oscuro";
+
+    public Automovil(){
+        this.id = ++ultimoId;
+    }
+
+    public Automovil(String fabricante, String modelo){
+        this();
+        this.fabricante = fabricante;
+        this.modelo = modelo;
+    }
+
+    public Automovil(String fabricante, String modelo, Color color){
+        this(fabricante,modelo);
+        this.color = color;
+    }
+
+    public Automovil(String fabricante, String modelo, Color color, double cilindrada) {
+        this(fabricante,modelo,color);
+        this.cilindrada = cilindrada;
+    }
+
+    public Automovil(String fabricante, String modelo, Color color, double cilindrada, int capacidadEstanque) {
+        this(fabricante,modelo,color,cilindrada);
+        this.capacidadEstanque = capacidadEstanque;
+    }
+
+    public void setFabricante(String fabricante){
+        this.fabricante = fabricante;
+    }
+
+    public String getFabricante(){
+        return this.fabricante;
+    }
+
+    public void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+
+    public String getModelo(){
+        return this.modelo;
+    }
+
+    public static void setCapacidadEstanqueEstatico(int capacidadEstanqueEstatico) {
+        Automovil.capacidadEstanqueEstatico = capacidadEstanqueEstatico;
+    }
+
+    public static int getCapacidadEstanqueEstatico() {
+        return capacidadEstanqueEstatico;
+    }
+
+    public static void setColorPatente(Color colorPatente){
+        Automovil.colorPatente = colorPatente;
+    }
+
+    public static Color getColorPatente() {
+        return colorPatente;
+    }
+
+    public void setColor(Color color){
+        this.color = color;
+    }
+
+    public Color getColor(){
+        return this.color;
+    }
+
+    public void setCilindrada(double cilindrada){
+        this.cilindrada = cilindrada;
+    }
+
+    public double getCilindrada(){
+        return this.cilindrada;
+    }
+
+    public void setCapacidadEstanque(int capacidadEstanque){
+        this.capacidadEstanque = capacidadEstanque;
+    }
+
+    public int getCapacidadEstanque(){
+        return this.capacidadEstanque;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String verDetalle(){
+        return "\nId: " + this.id + "\nFabricante: " + this.fabricante + "\nModelo: " +
+         this.modelo + "\nColor: " + this.color + "\nCilindrada: " + this.cilindrada +
+         "\npatenteColor: " +  colorPatente;
+    }
+
+    public String acelerar(int rpm){
+        return "el auto " + this.fabricante + " está acelerando a " + rpm + " rpm";
+    }
+
+    public String frenar(){
+        return this.fabricante + " " + this.modelo + " frenando...";
+    }
+
+    public String acelerarFrenar(int rpm){
+        String acelerar = this.acelerar(rpm);
+        String frenar = this.frenar();
+        return acelerar + "\n" + frenar;
+    }
+
+    public float calcularConsumo(int km, float porcentajeBencina){
+        return km/(capacidadEstanque*porcentajeBencina);
+    }
+
+    public float calcularConsumo(int km, int porcentajeBencina){
+        return km/(capacidadEstanque*(porcentajeBencina/100f));
+    }
+
+    public static float calcularConsumoEstatico(int km, int porcentajeBencina){
+        return km/(Automovil.capacidadEstanqueEstatico*(porcentajeBencina/100f));
+    }    
+
+    @Override
+    public boolean equals(Object obj) {
+            if(!(obj instanceof Automovil)){
+                return false;
+            }
+            if(this==obj){
+                return true;
+            }
+            Automovil a = (Automovil)obj;
+            return (this.getFabricante()!=null && this.getModelo()!=null && this.fabricante.equals(a.getFabricante()) && this.modelo.equals(a.getModelo()));
+    }
+
+    @Override
+    public String toString() {
+        return this.fabricante + " " + this.modelo;
+    }
+
+    
+    
+    
 }
--- src/ProgramacionOrientadaAObjetos/EjemploAutomovil.java
+++ src/ProgramacionOrientadaAObjetos/EjemploAutomovil.java
@@ -1,8 +1,42 @@
 package ProgramacionOrientadaAObjetos;
 
-public class EjemploAutomovil {
-    public static void main(String[] args) {
-        
-    }
+import java.util.Date;
 
+public class EjemploAutomovil {
+    @SuppressWarnings("unlikely-arg-type")
+    public static void main(String[] args) {
+        Automovil subaru = new Automovil("Subaru","Impreza");
+        subaru.setCilindrada(2.0);
+        subaru.setColor(Color.BLANCO);
+
+        Automovil mazda = new Automovil("Mazda","BT-50",Color.ROJO,3);
+        Automovil auto = new Automovil();
+        Date fecha = new Date();        
+
+        Automovil nissan = new Automovil("Nissan","Navara",Color.GRIS_OSCURO,3.5,50);
+        Automovil nissan2 = new Automovil("Nissan","Navara",Color.GRIS_OSCURO,3.5,50);
+        
+        System.out.println(auto.equals(fecha));
+        System.out.println(auto.equals(nissan2));
+        System.out.println("son iguales?: " + (nissan==nissan2));
+        System.out.println("son iguales?: " + (nissan.equals(nissan2)));
+
+     
+        System.out.println(subaru.verDetalle());
+        System.out.println(mazda.verDetalle());
+        System.out.println(nissan.verDetalle());
+
+        System.out.println(nissan);
+        System.out.println(nissan.toString());
+
+        System.out.println(subaru.acelerar(3000));
+        System.out.println(subaru.frenar());
+
+        System.out.println(mazda.acelerarFrenar(6000));
+
+        System.out.println("Automovil.getColorPatente() = " + Automovil.getColorPatente());
+        System.out.println("Kilómetros por litro del auto " + subaru.getModelo()+ " : " + subaru.calcularConsumo(300, 0.6f));
+        System.out.println("Kilómetros por litro del auto " + mazda.getModelo()+ " : " + mazda.calcularConsumo(300, 60));
+        System.out.println("Kilómetros por litro del auto " + nissan.getModelo()+ " : " + nissan.calcularConsumo(300, 60));
+    }
 }
