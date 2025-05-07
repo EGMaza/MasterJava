d4e23cdb9749b3ddb675419aadba70e8b60ac691
Eduardo Glez <eduardo_gonzalez_maza@hotmail.com>
2025-05-07 08:33:16 -0600

    Se agrega contador con bucle for

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        for(int i=0;i<10;i++){
+            System.out.println("Contador = " + (i+1));
+        }
     }
 }
