456e46f2a784a7969949cf13029fcd12a5078786
Eduardo Glez <eduardo_gonzalez_maza@hotmail.com>
2025-05-07 08:39:43 -0600

    se cambia el bucle for por un bucle while

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
-        for(int i=0;i<10;i++){
-            System.out.println("Contador = " + (i+1));
+        int i=0;
+        while(i<10){
+
+            i++;
+            System.out.println("Contador = " + i);
         }
     }
 }
