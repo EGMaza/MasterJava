15a6ff56651a665b1f221d1bbcdf280fcc08443b
Eduardo González <eduardo_gonzalez_maza@hotmail.com>
2025-09-17 00:54:48 -0600

    Actualización del repo local con la rama remota 'Hilos' y modificación de prueba

--- src/org/egmaza/hilos/ejemplos/EjemploExtenderThread.java
+++ src/org/egmaza/hilos/ejemplos/EjemploExtenderThread.java
@@ -5,7 +5,7 @@ import org.egmaza.hilos.ejemplos.threads.NombreThread;
 public class EjemploExtenderThread {
     public static void main(String[] args) throws InterruptedException {
 
-        Thread hilo = new NombreThread("Hilo1");
+        Thread hilo = new NombreThread("HiloEjemplo1");
         hilo.start();
 
         Thread hilo2 = new NombreThread("Hilo2");
