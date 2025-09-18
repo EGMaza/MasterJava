e52e469a732d5b38cad0d9b34ba085ad88185b5b
Eduardo González <eduardo_gonzalez_maza@hotmail.com>
2025-09-18 16:40:44 -0600

    Se crearon las clases 'EjemploProductorConsumidor', 'Panaderia', 'Consumidor' y 'Panadero'

---
+++ src/org/egmaza/hilos/ejemplosync/EjemploProductorConsumidor.java
@@ -0,0 +1,12 @@
+package org.egmaza.hilos.ejemplosync;
+
+import org.egmaza.hilos.ejemplosync.runnable.Consumidor;
+import org.egmaza.hilos.ejemplosync.runnable.Panadero;
+
+public class EjemploProductorConsumidor {
+    public static void main(String[] args) {
+        Panaderia p = new Panaderia();
+        new Thread(new Panadero(p)).start();
+        new Thread(new Consumidor(p)).start();
+    }
+}
