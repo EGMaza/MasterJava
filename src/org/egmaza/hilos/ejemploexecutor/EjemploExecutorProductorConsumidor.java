8087bff49af70468606d19b5b658ea2d5b1ae3e7
Eduardo González <eduardo_gonzalez_maza@hotmail.com>
2025-09-23 11:05:31 -0600

    Se creó la clase 'EjemploExecutorProductorConsumidor'

---
+++ src/org/egmaza/hilos/ejemploexecutor/EjemploExecutorProductorConsumidor.java
@@ -0,0 +1,27 @@
+package org.egmaza.hilos.ejemploexecutor;
+
+import org.egmaza.hilos.ejemplosync.Panaderia;
+import org.egmaza.hilos.ejemplosync.runnable.Consumidor;
+import org.egmaza.hilos.ejemplosync.runnable.Panadero;
+
+import java.util.concurrent.*;
+
+public class EjemploExecutorProductorConsumidor {
+    public static void main(String[] args) throws InterruptedException, ExecutionException, TimeoutException {
+        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(2);
+
+        System.out.println("Tamaño del pool: " + executor.getPoolSize());
+        System.out.println("Cantidad de tareas en cola: " + executor.getQueue().size());
+
+        Panaderia p = new Panaderia();
+        Runnable productor = new Panadero(p);
+        Runnable consumidor = new Consumidor(p);
+        Future<?> futuro1 = executor.submit(productor);
+        Future<?> futuro2 = executor.submit(consumidor);
+
+        System.out.println("Tamaño del pool: " + executor.getPoolSize());
+        System.out.println("Cantidad de tareas en cola: " + executor.getQueue().size());
+        executor.shutdown();
+        System.out.println("Continuando con la ejecución del método main 1");
+    }
+}
