09cfb8b3b282a174bf0faf1959ae576dab06117b
Eduardo González <eduardo_gonzalez_maza@hotmail.com>
2025-09-23 19:14:13 -0600

    Se creó la clase 'EjemploConsumer' además del paquete 'org.egmaza.java8.lambda.models' y dentro la clase 'Usuario'

--- src/org/egmaza/java8/lambda/EjemploConsumer.java
+++ src/org/egmaza/java8/lambda/EjemploConsumer.java
@@ -1,9 +1,14 @@
 package org.egmaza.java8.lambda;
 
+import org.egmaza.java8.lambda.models.Usuario;
+
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class EjemploConsumer {
     public static void main(String[] args) {
@@ -21,5 +26,28 @@ public class EjemploConsumer {
 
         consumidorBi.accept("Marco", 25);
 
+        Consumer<String> consumidor2 = System.out::println;
+        consumidor2.accept("Hola mundo lambda");
+
+        List<String> nombres = Arrays.asList("Andres", "Pepe", "Luz", "Paco");
+        nombres.forEach(consumidor2);
+
+        Supplier<Usuario> creaUsuario = Usuario::new;
+        Usuario usuario = creaUsuario.get();
+
+        //BiConsumer<Usuario, String> asignarNombre = (persona, nombre) -> {
+        //  persona.setNombre(nombre);
+        //};
+        BiConsumer<Usuario, String> asignarNombre = Usuario::setNombre;
+
+        asignarNombre.accept(usuario, "Andrés");
+        System.out.println("Nombre del usuario: " + usuario.getNombre());
+
+        //Supplier<String> proveedor = () -> {
+        //    return "Hola mundo lambda supplier";
+        //};
+        Supplier<String> proveedor = () -> "Hola mundo lambda supplier 2";
+
+        System.out.println(proveedor.get());
     }
 }
