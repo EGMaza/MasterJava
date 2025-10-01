a73c6ea2ffb19384a2a0c8f662af2ca3d0e83fe8
Eduardo González <eduardo_gonzalez_maza@hotmail.com>
2025-09-30 18:47:06 -0600

    Se creó el paquete 'org.egmaza.api.stream.ejemplos.moldels' y dentro la clase 'Usuario'. También se adaptó la clase 'EjemploStreamMap' para convertir el Stream de arreglo de nombres a uno de objetos Usuario

--- src/org/egmaza/api/stream/ejemplos/EjemploStreamMap.java
+++ src/org/egmaza/api/stream/ejemplos/EjemploStreamMap.java
@@ -1,5 +1,7 @@
 package org.egmaza.api.stream.ejemplos;
 
+import org.egmaza.api.stream.ejemplos.models.Usuario;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -10,13 +12,18 @@ public class EjemploStreamMap {
 
     public static void main(String[] args) {
 
-        Stream<String> nombres = Stream.of("Pato", "Paco", "Pepa", "Pepe")
+        Stream<Usuario> nombres = Stream.of("Pato Guzmán", "Paco González", "Pepa Gutierrez", "Pepe Mena")
                 .map(nombre -> {
-                    return nombre.toUpperCase();
+                    return new Usuario(nombre.split(" ")[0], nombre.split(" ")[1]);
                 })
-                .peek(System.out::println)
-                .map(String::toLowerCase);
-        List<String> lista = nombres.collect(Collectors.toList());
+                .peek(Usuario -> System.out.println(Usuario.getNombre()))
+                .map( usuario ->{
+                    String nombre =  usuario.getNombre().toUpperCase();
+                    usuario.setNombre(nombre);
+                    return usuario;
+                });
+        List<Usuario> lista = nombres.collect(Collectors.toList());
+        //lista.forEach(Usuario -> System.out.println(Usuario.getApellido()));
         lista.forEach(System.out::println);
         //nombres.forEach(System.out::println);
     }
