acd3dd753238a1ec3ee8ae699c9c4459a929691a
Eduardo González <eduardo_gonzalez_maza@hotmail.com>
2025-09-30 19:10:11 -0600

    Se creó la clase 'EjemploStreamFilterSingle2' y se le agregaron los atributos 'id' y 'ultimoId' a la clase 'Usuario'

---
+++ src/org/egmaza/api/stream/ejemplos/EjemploStreamFilterSingle2.java
@@ -0,0 +1,23 @@
+package org.egmaza.api.stream.ejemplos;
+
+import org.egmaza.api.stream.ejemplos.models.Usuario;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class EjemploStreamFilterSingle2 {
+
+    public static void main(String[] args) {
+
+        Usuario usuario = Stream.of("Pato Guzmán",
+                        "Paco González", "Pepa Gutierrez", "Pepe Mena",
+                "Pepe García")
+                .map(nombre -> {
+                    return new Usuario(nombre.split(" ")[0], nombre.split(" ")[1]);
+                })
+                .filter(u -> u.getId().equals(5))
+                .findFirst().get();
+
+        System.out.println(usuario);
+    }
+}
