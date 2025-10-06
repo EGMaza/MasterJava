debf4eed12515e868a89102aa43da7d5c9b80f94
Eduardo González <eduardo_gonzalez_maza@hotmail.com>
2025-10-06 15:21:58 -0600

    Se creó la clase 'Factura' y se le agregó el atributo privado List<Factura> a la clase 'Usuario'

---
+++ src/org/egmaza/api/stream/ejemplos/models/Factura.java
@@ -0,0 +1,14 @@
+package org.egmaza.api.stream.ejemplos.models;
+
+public class Factura {
+
+    private String descripcion;
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
--- src/org/egmaza/api/stream/ejemplos/models/Usuario.java
+++ src/org/egmaza/api/stream/ejemplos/models/Usuario.java
@@ -1,5 +1,7 @@
 package org.egmaza.api.stream.ejemplos.models;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class Usuario {
@@ -8,11 +10,21 @@ public class Usuario {
     private String apellido;
     private Integer id;
     private static int ultimoId;
+    private List<Factura> facturas;
 
     public Usuario(String nombre, String apellido) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.id = ++ultimoId;
+        this.facturas = new ArrayList<>();
+    }
+
+    public List<Factura> getFacturas() {
+        return facturas;
+    }
+
+    public void addFactura(Factura factura) {
+        this.facturas.add(factura);
     }
 
     public Integer getId() {
