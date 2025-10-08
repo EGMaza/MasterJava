363e41ad0227e745719c7ecfead1d00174cd318c
Eduardo González <eduardo_gonzalez_maza@hotmail.com>
2025-10-08 00:45:19 -0600

    Se creó el paquete 'org.egmaza.optional.ejemplo' y dentro la clase 'EjemploOptional'

---
+++ src/org/egmaza/optional/ejemplo/EjemploOptional.java
@@ -0,0 +1,37 @@
+package org.egmaza.optional.ejemplo;
+
+import java.util.Optional;
+
+public class EjemploOptional {
+    static void main() {
+
+        String nombre = "Eduardo";
+        Optional<String> opt = Optional.of(nombre);
+        System.out.println("opt = " + opt);
+        System.out.println(opt.isPresent());
+        if(opt.isPresent()) {
+            System.out.println("Hola " + opt.get());
+        }
+        System.out.println(opt.isEmpty());
+
+        opt.ifPresent( valor -> System.out.println("Hola " + valor));
+        //nombre = null;
+        opt = Optional.ofNullable(nombre);
+        System.out.println("opt = " + opt);
+        System.out.println(opt.isPresent());
+        System.out.println(opt.isEmpty());
+        opt.ifPresentOrElse(valor -> System.out.println("Hola " + valor),
+                () -> System.out.println("No está presente"));
+
+        if(opt.isPresent()) {
+            System.out.println("Hola " + opt.get());
+        }
+        else{
+            System.out.println("No está presente");
+        }
+
+        Optional<String> optEmpty = Optional.empty();
+        System.out.println("optEmpty = " + optEmpty);
+        System.out.println(optEmpty.isPresent());
+    }
+}
