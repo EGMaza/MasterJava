e7b9d39a1dfba78c726d695db0dd7fd07fd901b6
Eduardo González <eduardo_gonzalez_maza@hotmail.com>
2025-10-16 16:04:26 -0600

    Se modificó la clase 'EjemploLocalDate' para agregar unas opciones para obtener los nombres de mes y día en idioma español

--- src/org/egmaza/datetime/ejemplos/EjemploLocalDate.java
+++ src/org/egmaza/datetime/ejemplos/EjemploLocalDate.java
@@ -3,7 +3,9 @@ package org.egmaza.datetime.ejemplos;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.format.TextStyle;
 import java.time.temporal.ChronoUnit;
+import java.util.Locale;
 
 public class EjemploLocalDate {
 
@@ -12,7 +14,15 @@ public class EjemploLocalDate {
         LocalDate fechaActual = LocalDate.now();
         System.out.println("fechaActual = " + fechaActual);
         System.out.println("Dia: " + fechaActual.getDayOfMonth());
-        System.out.println("Mes: " + fechaActual.getMonth());
+        Month mes = fechaActual.getMonth();
+        System.out.println("Mes: " + mes);
+        System.out.println("Número del mes: " + mes.getValue());
+        System.out.println("Mes en español: " + mes.getDisplayName(TextStyle.FULL, new Locale("es", "ES")));
+
+        DayOfWeek diaSemana = fechaActual.getDayOfWeek();
+        System.out.println("Número del día: " + diaSemana.getValue());
+        System.out.println("Nombre del día: " + diaSemana.getDisplayName(TextStyle.FULL, new Locale("es", "ES")));
+
         System.out.println("Año: " + fechaActual.getYear());
         System.out.println("Dia del año: "  + fechaActual.getDayOfYear());
         System.out.println("Era: " + fechaActual.getEra());
