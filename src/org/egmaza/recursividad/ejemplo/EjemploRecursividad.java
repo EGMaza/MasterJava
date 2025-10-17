b8996339c68383831023fbb988986a7123c738ed
Eduardo González <eduardo_gonzalez_maza@hotmail.com>
2025-10-17 17:40:44 -0600

    Se modificaron las clases 'EjemploRecursividad' y 'Componente' para hacer que el método recursivo se construyera con el api Stream de java 8

--- src/org/egmaza/recursividad/ejemplo/EjemploRecursividad.java
+++ src/org/egmaza/recursividad/ejemplo/EjemploRecursividad.java
@@ -2,6 +2,8 @@ package org.egmaza.recursividad.ejemplo;
 
 import org.egmaza.recursividad.ejemplo.models.Componente;
 
+import java.util.stream.Stream;
+
 public class EjemploRecursividad {
 
     public static void main(String[] args) {
@@ -38,6 +40,23 @@ public class EjemploRecursividad {
                 .addComponente(new Componente("Teclado"))
                 .addComponente(new Componente("Mouse"));
 
-        System.out.println("PC: " + pc.toString());
+        metodoRecursivoJava8(pc, 0)
+                .forEach(c -> System.out.println("\t".repeat(c.getNivel())
+        + c.getNombre()));
+    }
+
+    public static Stream<Componente> metodoRecursivoJava8(Componente c, int nivel){
+        c.setNivel(nivel);
+        return Stream.concat(Stream.of(c),c.getHijos().stream()
+                .flatMap(hijo -> metodoRecursivoJava8(hijo, nivel + 1)));
+    }
+
+    public static void metodoRecursivo(Componente c, int nivel){
+        System.out.println("\t".repeat(nivel) + c.getNombre());
+        if(c.tieneHijos()){
+            for(Componente hijo:c.getHijos()){
+                metodoRecursivo(hijo, nivel+1);
+            }
+        }
     }
 }
