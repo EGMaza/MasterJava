a59db70b8bd2e833ffa492e58290798154b00923
Gonzalez Maza <egmaza@minsait.com>
2025-10-20 10:05:42 -0600

    Se crearon las clases 'PizzeriaCaliforniaFactory', 'PizzaCaliforniaVegetariana', 'PizzaCaliforniaPepperoni' y 'PizzaCaliforniaQueso', además se creó el paquete 'org.egmaza.patrones.factory.ejemplo' y dentro la clase 'EjemploFactory'

---
+++ src/org/egmaza/patrones/factory/producto/PizzaCaliforniaPepperoni.java
@@ -0,0 +1,27 @@
+package org.egmaza.patrones.factory.producto;
+
+import org.egmaza.patrones.factory.PizzaProducto;
+
+public class PizzaCaliforniaPepperoni extends PizzaProducto {
+
+    public PizzaCaliforniaPepperoni() {
+        super();
+        nombre = "Pizza California Pepperoni";
+        masa = "Masa a la piedra gruesa";
+        ingredientes.add("Pepperoni");
+        ingredientes.add("Extra queso mozzarella");
+        ingredientes.add("Aceitunas");
+    }
+
+    @Override
+    public void cocinar() {
+        System.out.println("Cocinando por 50 min. a 55°C");
+    }
+
+    @Override
+    public void cortar() {
+        System.out.println("Cortando la pizza en pequeños rectángulos");
+    }
+
+
+}
---
+++ src/org/egmaza/patrones/factory/producto/PizzaCaliforniaVegetariana.java
@@ -0,0 +1,28 @@
+package org.egmaza.patrones.factory.producto;
+
+import org.egmaza.patrones.factory.PizzaProducto;
+
+public class PizzaCaliforniaVegetariana extends PizzaProducto {
+
+    public PizzaCaliforniaVegetariana(){
+        super();
+        nombre = "Pizza California Vegetariana";
+        masa = "Masa delgada Light";
+        salsa = "Salsa BBQ light";
+        ingredientes.add("Queso Mozzarella");
+        ingredientes.add("Aceitunas");
+        ingredientes.add("Espinacas");
+        ingredientes.add("Cebolla");
+        ingredientes.add("Berenjena");
+    }
+
+    @Override
+    public void cocinar() {
+        System.out.println("Cocinando por 20 min a 180°C");
+    }
+
+    @Override
+    public void cortar() {
+        System.out.println("Cortando la pizza en rebanadas rectangulares");
+    }
+}
