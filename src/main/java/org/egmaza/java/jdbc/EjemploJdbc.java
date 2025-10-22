c8bb3da46158e7a27527753fb2ee9bdde89ee3b3
EGMazaCompuTrabajo <eduardo.gonzalez.maza1@gmail.com>
2025-10-22 14:09:02 -0600

    Se creó el paquete 'org.egmaza.java.jdbc.util' y dentro la clase 'ConexionBaseDatos', además se modificó la clase 'EjemploJdbc' para que pudiera generar una instancia singleton de conexion a base de datos de la clase recien creada (los archivos referentes a la base de datos no se incluyen en este repositorio)

--- src/main/java/org/egmaza/java/jdbc/EjemploJdbc.java
+++ src/main/java/org/egmaza/java/jdbc/EjemploJdbc.java
@@ -1,17 +1,14 @@
 package org.egmaza.java.jdbc;
 
+import org.egmaza.java.jdbc.util.ConexionBaseDatos;
+
 import java.sql.*;
 
 public class EjemploJdbc {
 
     public static void main(String[] args) {
 
-        String url = "jdbc:mysql://localhost:3306/java_curso?serverTimezone=America/Mexico_City";
-        String username = "root";
-        String password = "123456";
-        final String stmt2 = "INSERT INTO productos (nombre, precio, fecha_registro) VALUES ('Coca Cola 600ml', 21, 2025-10-23)";
-
-        try (Connection conn = DriverManager.getConnection(url, username, password);
+        try (Connection conn = ConexionBaseDatos.getInstance();
              Statement stmt = conn.createStatement();
              ResultSet resultado = stmt.executeQuery("SELECT * FROM productos")){
 
