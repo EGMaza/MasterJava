4cb88bae90e99dc6370361ee522d081c22c00416
EGMazaCompuTrabajo <eduardo.gonzalez.maza1@gmail.com>
2025-10-23 18:46:26 -0600

    Se creó la clase 'Window08Calculator'

---
+++ src/org/egmaza/swing/Window08Calculator.java
@@ -0,0 +1,102 @@
+package org.egmaza.swing;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Window08Calculator extends JFrame implements ActionListener {
+
+    Container container;
+    JTextField numberA, numberB, result;
+
+    public static final String CALCULATOR_ADD = "Sumar";
+    public static final String CALCULATOR_SUBSTRACTION = "Restar";
+    public static final String CALCULATOR_MULTIPLICATION = "Multiplicar";
+    public static final String CALCULATOR_DIVISION = "Dividir";
+    public static final String CALCULATOR_CLOSE = "Salir";
+
+    public Window08Calculator() throws HeadlessException {
+        super("Calculadora");
+        container = getContentPane();
+        container.setLayout(new BorderLayout());
+        JMenuBar bar = new JMenuBar();
+        setJMenuBar(bar);
+        JMenu menu = new JMenu("Operaciones");
+
+        JMenuItem itemAdd = new JMenuItem(CALCULATOR_ADD);
+        JMenuItem itemSus = new JMenuItem(CALCULATOR_SUBSTRACTION);
+        JMenuItem itemDiv = new JMenuItem(CALCULATOR_DIVISION);
+        JMenuItem itemMult = new JMenuItem(CALCULATOR_MULTIPLICATION);
+
+        menu.add(itemAdd);
+        menu.add(itemSus);
+        menu.add(itemDiv);
+        menu.add(itemMult);
+
+        JMenu closeMenu = new JMenu("Cerrar");
+        JMenuItem itemClose = new JMenuItem(CALCULATOR_CLOSE);
+        closeMenu.add(itemClose);
+
+        bar.add(menu);
+        bar.add(closeMenu);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+        panel.add(new JLabel("Número 1"));
+        panel.add(numberA = new JTextField(3));
+        panel.add(new JLabel("Número 2"));
+        panel.add(numberB = new JTextField(3));
+        panel.add(new JLabel("Resultado"));
+        panel.add(result = new JTextField(5));
+        result.setEditable(false);
+        //result.setEnabled(false);
+
+        container.add(panel, BorderLayout.CENTER);
+
+        itemAdd.addActionListener(this);
+        itemSus.addActionListener(this);
+        itemDiv.addActionListener(this);
+        itemMult.addActionListener(this);
+        itemClose.addActionListener(this);
+
+        setVisible(true);
+        pack();
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+    }
+
+    public static void main(String[] args) {
+
+        new Window08Calculator();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String operation = e.getActionCommand();
+        int a = Integer.parseInt(numberA.getText());
+        int b = Integer.parseInt(numberB.getText());
+        int value = 0;
+
+        switch (operation){
+            case CALCULATOR_ADD:
+                value = a +b;
+                break;
+            case CALCULATOR_SUBSTRACTION:
+                value = a - b;
+                break;
+            case CALCULATOR_MULTIPLICATION:
+                value = a * b;
+                break;
+            case CALCULATOR_DIVISION:
+                if(b==0){
+                    throw new ArithmeticException("No se puede dividir entre cero, infinito");
+                }
+                value = a/b;
+                break;
+            case CALCULATOR_CLOSE:
+                System.exit(0);
+        }
+        result.setText(String.valueOf(value));
+    }
+}
