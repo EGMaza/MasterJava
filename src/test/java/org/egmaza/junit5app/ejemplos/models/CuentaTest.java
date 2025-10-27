97897eee4801b27a3ffad0d1d412c03806bc415e
EGMazaCompuTrabajo <eduardo.gonzalez.maza1@gmail.com>
2025-10-27 17:20:26 -0600

    Se crearon las clases 'DineroInsuficienteException' y 'Banco', además se modificaron las clases 'Cuenta' y 'CuentaTest' para implementar la nueva clase de excepción y nuevas pruebas unitarias

--- src/test/java/org/egmaza/junit5app/ejemplos/models/CuentaTest.java
+++ src/test/java/org/egmaza/junit5app/ejemplos/models/CuentaTest.java
@@ -1,5 +1,6 @@
 package org.egmaza.junit5app.ejemplos.models;
 
+import org.egmaza.junit5app.ejemplos.exceptions.DineroInsuficienteException;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
@@ -53,5 +54,46 @@ class CuentaTest {
         assertNotNull(cuenta.getSaldo());
         assertEquals(1100, cuenta.getSaldo().intValue());
         assertEquals("1100.12345", cuenta.getSaldo().toPlainString());
+    }
+
+    @Test
+    void testDineroInsuficienteExceptionCuenta(){
+        Cuenta cuenta = new Cuenta("Andres", new BigDecimal("1000.12345"));
+        Exception exception = assertThrows(DineroInsuficienteException.class, (() -> {
+            cuenta.debito(new BigDecimal(1500));
+        }));
+
+        String actual = exception.getMessage();
+        String esperado = "Dinero Insuficiente";
+        assertEquals(esperado, actual);
+    }
+
+    @Test
+    void testTransferirDineroCuentas(){
+        Cuenta cuenta1 = new Cuenta("John Doe", new BigDecimal("2500"));
+        Cuenta cuenta2 = new Cuenta("Andres Smith", new BigDecimal("1500.8989"));
+
+        Banco banco = new Banco();
+        banco.setNombre("Banco del estado");
+        banco.transferir(cuenta2, cuenta1, new BigDecimal(500));
+        assertEquals("1000.8989", cuenta2.getSaldo().toPlainString());
+        assertEquals("3000", cuenta1.getSaldo().toPlainString());
+    }
+
+    @Test
+    void testRelacionBancoCuentas(){
+        Cuenta cuenta1 = new Cuenta("John Doe", new BigDecimal("2500"));
+        Cuenta cuenta2 = new Cuenta("Andres Smith", new BigDecimal("1500.8989"));
+
+        Banco banco = new Banco();
+        banco.addCuenta(cuenta1);
+        banco.addCuenta(cuenta2);
+
+        banco.setNombre("Banco del estado");
+        banco.transferir(cuenta2, cuenta1, new BigDecimal(500));
+        assertEquals("1000.8989", cuenta2.getSaldo().toPlainString());
+        assertEquals("3000", cuenta1.getSaldo().toPlainString());
+
+        assertEquals(2, banco.getCuentas().size());
     }
 }
