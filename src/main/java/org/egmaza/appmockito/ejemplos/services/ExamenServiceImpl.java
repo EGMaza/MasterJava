6cfbb0aef459eb27a915e802619d869f2106e974
EGMazaCompuTrabajo <eduardo.gonzalez.maza1@gmail.com>
2025-10-29 09:36:28 -0600

    Se crearon las clases 'ExamenRepositoryImpl' y 'ExamenServiceImpl' como parte de la implementación que se usará de ejemplo y se creó la clase 'ExamenServiceImplTest' en la que se estarán implementando los diferentes test que se generen

---
+++ src/main/java/org/egmaza/appmockito/ejemplos/services/ExamenServiceImpl.java
@@ -0,0 +1,28 @@
+package org.egmaza.appmockito.ejemplos.services;
+
+import org.egmaza.appmockito.ejemplos.models.Examen;
+import org.egmaza.appmockito.ejemplos.repositories.ExamenRepository;
+
+import java.util.Optional;
+
+public class ExamenServiceImpl implements ExamenService{
+
+    private ExamenRepository examenRepository;
+
+    public ExamenServiceImpl(ExamenRepository examenRepository) {
+        this.examenRepository = examenRepository;
+    }
+
+    @Override
+    public Examen findExamenPorNombre(String nombre) {
+        Optional<Examen> examenOptional = examenRepository.findAll()
+                .stream()
+                .filter(e -> e.getNombre().contains(nombre))
+                .findFirst();
+        Examen examen = null;
+        if(examenOptional.isPresent()){
+            examen = examenOptional.orElseThrow();
+        }
+        return examen;
+    }
+}
