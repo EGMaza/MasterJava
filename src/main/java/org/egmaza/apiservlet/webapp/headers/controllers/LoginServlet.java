01e746987ea7897e134ce26ca72f14957d179553
EGMazaCompuTrabajo <eduardo.gonzalez.maza1@gmail.com>
2025-11-03 18:45:41 -0600

    Se crearon las clases 'LogoutServlet', 'LoginServlet', 'ProductoServlet', 'LoginService', 'LoginServiceImpl' y el archivo 'login.jsp' para ejemplificar el manejo de cookies

--- src/main/java/org/egmaza/apiservlet/webapp/headers/controllers/LoginServlet.java
+++ src/main/java/org/egmaza/apiservlet/webapp/headers/controllers/LoginServlet.java
@@ -2,18 +2,50 @@ package org.egmaza.apiservlet.webapp.headers.controllers;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.egmaza.apiservlet.webapp.headers.services.LoginServiceImpl;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Optional;
 
-@WebServlet("/login")
+@WebServlet({"/login", "/login.html"})
 public class LoginServlet extends HttpServlet {
 
     final static String USERNAME = "admin";
     final static String PASSWORD  = "123456";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Cookie[] cookies = req.getCookies() != null ? req.getCookies() : new Cookie[0];
+        Optional<String> cookieOptional = new LoginServiceImpl().getUsername(req);
+
+        if (cookieOptional.isPresent()) {
+            resp.setContentType("text/html;charset=UTF-8");
+
+            try (PrintWriter out = resp.getWriter()) {
+
+                out.println("<!DOCTYPE html>");
+                out.println("<html>");
+                out.println("   <head>");
+                out.println("   <meta charset=\"UTF-8\">");
+                out.println("   <title>Hola " + cookieOptional.get() + "</title>");
+                out.println("   </head>");
+                out.println("   <body>");
+                out.println("      <h1>Hola " + cookieOptional.get() + " has iniciado sesión con éxito</h1>");
+                out.println("      <p><a href='"+ req.getContextPath() +"/index.html'>volver</a></p>");
+                out.println("      <p><a href='"+ req.getContextPath() +"/logout'>cerrar sesión</a></p>");
+                out.println("   </body>");
+                out.println("</html>");
+            }
+        } else {
+            getServletContext().getRequestDispatcher("/login.jsp").forward(req, resp);
+        }
+    }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -21,25 +53,14 @@ public class LoginServlet extends HttpServlet {
         String password = req.getParameter("password");
 
         if(USERNAME.equals(username) && PASSWORD.equals(password)){
-            resp.setContentType("text/html;charset=UTF-8");
+            Cookie usernameCookie = new Cookie("username", username);
+            resp.addCookie(usernameCookie);
 
-            try(PrintWriter out = resp.getWriter()){
-
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("   <head>");
-            out.println("   <meta charset=\"UTF-8\">");
-            out.println("   <title>Login correcto</title>");
-            out.println("   </head>");
-            out.println("   <body>");
-            out.println("      <h1>Login correcto</h1>");
-            out.println("<h3>Hola " + username + ", has iniciado sesión con éxito</h3>");
-            out.println("   </body>");
-            out.println("</html>");
-            }
+            resp.sendRedirect(req.getContextPath()+"/login");
         }
         else{
             resp.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Usuario y/o contraseña incorrectos: " + username + ", " + password);
         }
     }
+
 }
--- src/main/java/org/egmaza/apiservlet/webapp/headers/controllers/ProductoServlet.java
+++ src/main/java/org/egmaza/apiservlet/webapp/headers/controllers/ProductoServlet.java
@@ -2,16 +2,20 @@ package org.egmaza.apiservlet.webapp.headers.controllers;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.egmaza.apiservlet.webapp.headers.models.Producto;
+import org.egmaza.apiservlet.webapp.headers.services.LoginServiceImpl;
 import org.egmaza.apiservlet.webapp.headers.services.ProductoService;
 import org.egmaza.apiservlet.webapp.headers.services.ProductoServiceImpl;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @WebServlet({"/productos.html", "/productos"})
 public class ProductoServlet extends HttpServlet {
@@ -20,6 +24,16 @@ public class ProductoServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ProductoService service = new ProductoServiceImpl();
         List<Producto> productos = service.listar();
+
+        Cookie[] cookies = req.getCookies() != null ? req.getCookies() : new Cookie[0];
+
+        Optional<String> cookieOptional = new LoginServiceImpl().getUsername(req);
+
+        Optional<String> cookieOptional2 = Arrays.stream(cookies)
+                .filter(c -> "username".equals(c.getName()))
+                .map(Cookie::getValue)
+                .findFirst();
+
         resp.setContentType("text/html;charset=UTF-8");
 
         try (PrintWriter out = resp.getWriter()) {
@@ -32,19 +46,27 @@ public class ProductoServlet extends HttpServlet {
             out.println("   </head>");
             out.println("   <body>");
             out.println("      <h1>Listado de Productos!</h1>");
-
+            if(cookieOptional.isPresent()){
+                out.println("<div style='color: blue;'>Hola " + cookieOptional.get() + " bienvenido</div>");
+            }
             out.println("      <table>");
             out.println("         <tr>");
             out.println("         <th>ID</th>");
             out.println("         <th>Nombre</th>");
             out.println("         <th>Tipo</th>");
-            out.println("         <th>Precio</th>");
+            if(cookieOptional.isPresent()){
+                out.println("         <th>Precio</th>");
+            }
             out.println("         <tr>");
-            productos.forEach(p -> out.println("<td>" + p.getId() + "</td>\n" +
-                    "<td>" + p.getNombre() + "</td>\n" +
-                    "<td>" + p.getTipo() + "</td>\n" +
-                    "<td>" + p.getPrecio() + "</td>\n<tr>"
-            ));
+            productos.forEach(p -> {
+                out.println("<td>" + p.getId() + "</td>\n");
+                out.println("<td>" + p.getNombre() + "</td>\n");
+                out.println("<td>" + p.getTipo() + "</td>\n");
+                if(cookieOptional.isPresent()){
+                    out.println("<td>" + p.getPrecio() + "</td>\n");
+                }
+                out.println("</tr>");
+            });
             out.println("      </table>");
 
             out.println("   </body>");
