10a61f1f44b642335444985fb2eecb6c120c135c
EGMazaCompuTrabajo <eduardo.gonzalez.maza1@gmail.com>
2025-11-11 18:31:40 -0600

    Se crearon la clase 'ConexionBaseDatosDS' y los archivos 'context.xml' y 'web.xml' y se modificó la clase 'ConexionFilter', todo para implementar un pool de conexiones JDBC con Tomcat Datasource

--- src/main/java/org/egmaza/apiservlet/webapp/headers/filters/ConexionFilter.java
+++ src/main/java/org/egmaza/apiservlet/webapp/headers/filters/ConexionFilter.java
@@ -4,8 +4,9 @@ import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletResponse;
 import org.egmaza.apiservlet.webapp.headers.services.ServiceJdbcException;
-import org.egmaza.apiservlet.webapp.headers.util.ConexionBaseDatos;
+import org.egmaza.apiservlet.webapp.headers.util.ConexionBaseDatosDS;
 
+import javax.naming.NamingException;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -16,7 +17,7 @@ public class ConexionFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
-        try(Connection conn = ConexionBaseDatos.getConnection()){
+        try(Connection conn = ConexionBaseDatosDS.getConnection()){
 
             if(conn.getAutoCommit()) {
                 conn.setAutoCommit(false);
@@ -32,7 +33,7 @@ public class ConexionFilter implements Filter {
                 e.printStackTrace();
             }
 
-        } catch (SQLException e) {
+        } catch (SQLException | NamingException e) {
             e.printStackTrace();
         }
     }
