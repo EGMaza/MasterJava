ddc06d4e07d2d236d0036f9b99aa6459f8422257
EGMazaCompuTrabajo <eduardo.gonzalez.maza1@gmail.com>
2025-11-13 17:57:00 -0600

    Se creó el archivo 'beans.xml' como parte de la configuración para activar la inyección de dependencias en el proyecto, y se modificaron las clases y vistas relacionadas al carro de compras para que ahora funcionaran con inyección de dependencias

--- src/main/java/org/egmaza/cdi/headers/controllers/AgregarCarroServlet.java
+++ src/main/java/org/egmaza/cdi/headers/controllers/AgregarCarroServlet.java
@@ -1,5 +1,6 @@
 package org.egmaza.cdi.headers.controllers;
 
+import jakarta.inject.Inject;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -18,6 +19,10 @@ import java.util.Optional;
 
 @WebServlet("/carro/agregar")
 public class AgregarCarroServlet extends HttpServlet {
+
+    @Inject
+    private Carro carro;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Long id = Long.parseLong(req.getParameter("id"));
@@ -26,8 +31,8 @@ public class AgregarCarroServlet extends HttpServlet {
         Optional<Producto> producto = service.porId(id);
         if(producto.isPresent()){
             ItemCarro item = new ItemCarro(1, producto.get());
-            HttpSession session = req.getSession();
-            Carro carro = (Carro) session.getAttribute("carro");
+            //HttpSession session = req.getSession();
+            //Carro carro = (Carro) session.getAttribute("carro");
             carro.addItemCarro(item);
         }
         resp.sendRedirect(req.getContextPath()+"/carro/ver");
--- src/main/java/org/egmaza/cdi/headers/listeners/AplicacionListener.java
+++ src/main/java/org/egmaza/cdi/headers/listeners/AplicacionListener.java
@@ -41,9 +41,9 @@ public class AplicacionListener implements ServletContextListener,
     @Override
     public void sessionCreated(HttpSessionEvent se) {
         servletContext.log("Inicializando la sesión HTTP");
-        Carro carro = new Carro();
-        HttpSession session = se.getSession();
-        session.setAttribute("carro", carro);
+        //Carro carro = new Carro();
+        //HttpSession session = se.getSession();
+        //session.setAttribute("carro", carro);
     }
 
     @Override
