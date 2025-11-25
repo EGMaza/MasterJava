27c0f89096e15437997ce0c1eadc58666631cb2b
EGMazaCompuTrabajo <eduardo.gonzalez.maza1@gmail.com>
2025-11-25 16:31:40 -0600

    Se crearon las clases 'ClienteService' y 'ClienteServiceImpl' que representan el Servicio, como parte del crud

---
+++ src/main/java/org/egmaza/hibernateapp/services/ClienteService.java
@@ -0,0 +1,15 @@
+package org.egmaza.hibernateapp.services;
+
+import org.egmaza.hibernateapp.entity.Cliente;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ClienteService {
+
+    List<Cliente> listar();
+    Optional<Cliente> porId(Long id);
+    void guardar(Cliente cliente);
+    void eliminar(Long id);
+
+}
