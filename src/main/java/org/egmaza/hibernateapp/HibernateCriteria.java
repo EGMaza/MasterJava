c74a6c5f2944e71c2bc0b79abaa690c2e4eb6bdd
EGMazaCompuTrabajo <eduardo.gonzalez.maza1@gmail.com>
2025-11-27 17:07:46 -0600

    Se modificó la clase 'HibernateCriteria' para ejemplificar el uso de las clausulas order by, u un ejemplo con single result por id, ditsnct nombre y upper

--- src/main/java/org/egmaza/hibernateapp/HibernateCriteria.java
+++ src/main/java/org/egmaza/hibernateapp/HibernateCriteria.java
@@ -113,6 +113,20 @@ public class HibernateCriteria {
         nombres = em.createQuery(queryString).getResultList();
         nombres.forEach(System.out::println);
 
+        System.out.println("===== consulta por nombres y apellidos concatenados =====");
+        queryString = criteria.createQuery(String.class);
+        from = queryString.from(Cliente.class);
+        queryString.select(criteria.concat(criteria.concat(from.get("nombre"), " "),from.get("apellido")));
+        nombres = em.createQuery(queryString).getResultList();
+        nombres.forEach(System.out::println);
+
+        System.out.println("===== consulta por nombres y apellidos concatenados upper =====");
+        queryString = criteria.createQuery(String.class);
+        from = queryString.from(Cliente.class);
+        queryString.select(criteria.upper(criteria.concat(criteria.concat(from.get("nombre"), " "),from.get("apellido"))));
+        nombres = em.createQuery(queryString).getResultList();
+        nombres.forEach(System.out::println);
+
         em.close();
     }
 }
